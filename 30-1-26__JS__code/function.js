82d3ee183e06208f4717eee879b21161b4f79abb
ishwariyaraja <ishwariya0404@gmail.com>
2026-01-30 17:10:56 +0530

    sum

---
+++ 30-1-26__JS__code/function.js
@@ -0,0 +1,51 @@
+
+// function declration //
+ let result = add(10,20);
+
+  function add(a,b){
+    return a+b
+  }
+
+  console.log(result);
+ let total = value(100,100)
+ function value(price1, price2){
+    return price1+price2
+ }
+ console.log(total)
+ 
+// function declration //
+//  let result = add(10,20)
+//   function add(a,b){
+//     return a+b;
+//   }
+//   console.log(result);
+//  let result =total(100,100)
+
+//   function total(price1,price2){
+//     return price1+price2;
+//   }
+  
+//   console.log(result);
+//     let total = value(100,100)
+//  function value(price1, price2){
+//     return price1+price2
+//  }
+//  console.log(total)
+
+//  let total = function(price1, price2) {
+//   return price1 + price2;
+// };
+
+// console.log(total(100, 100));
+
+// let price =total(10,10)
+// function total(value1,value2){
+//     return value1+value2
+// }
+// console.log(price)
+//  let result =total(100,100)
+//  function total(value1,value2){
+//      return(value1+value2)
+//  }
+//  console.log(result)
+
